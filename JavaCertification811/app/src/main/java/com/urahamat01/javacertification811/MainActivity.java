25275a88aa6071ea065e986ce4a5126e671f464c
urahamat01 <urahamat01@gmail.com>
2019-12-25 02:12:36 +0600

    googleLogin

---
+++ JavaCertification811/app/src/main/java/com/urahamat01/javacertification811/MainActivity.java
@@ -0,0 +1,14 @@
+package com.urahamat01.javacertification811;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
